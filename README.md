de5230276a6c2e192fed924893945920eb86cbb9
Tim Redband <tim.redband@dunnhumby.com>
2024-09-09 09:55:47 -0400

    add inspiration for init

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Font
 
 CaskaydiaMono Nerd Font
+
+To get the ^M character, type Ctrl + V and hit Enter. Under Windows, do Ctrl + Q, Enter.
--- init.sh
+++ init.sh
@@ -10,3 +10,20 @@ git clone --bare git@github.com:timredband/dotfiles.git $HOME/.cfg
 config checkout
 
 # to push do config push --set-upstream origin master
+#
+#
+# inspired from this
+# git clone --bare https://bitbucket.org/durdn/cfg.git $HOME/.cfg
+# function config {
+#   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+# }
+# mkdir -p .config-backup
+# config checkout
+# if [ $? = 0 ]; then
+#   echo "Checked out config."
+# else
+#   echo "Backing up pre-existing dot files."
+#   config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+# fi
+# config checkout
+# config config status.showUntrackedFiles no
