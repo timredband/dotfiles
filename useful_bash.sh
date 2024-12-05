5915ee8ea3c77850c16c6553413ba10192840ea0
Tim Redband <tim.redband@dunnhumby.com>
2024-12-05 09:44:00 -0500

    add colored git branch to prompt

--- useful_bash.sh
+++ useful_bash.sh
@@ -39,3 +39,24 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 eval "$(zoxide init bash)"
+
+source ~/git-prompt.sh
+export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[1;31m\]\$(__git_ps1 \"(%s)\")\[\033[00m\]\$ "
+
+# Color mapping
+# grey='\[\033[1;30m\]'
+# red='\[\033[0;31m\]'
+# RED='\[\033[1;31m\]'
+# green='\[\033[0;32m\]'
+# GREEN='\[\033[1;32m\]'
+# yellow='\[\033[0;33m\]'
+# YELLOW='\[\033[1;33m\]'
+# purple='\[\033[0;35m\]'
+# PURPLE='\[\033[1;35m\]'
+# white='\[\033[0;37m\]'
+# WHITE='\[\033[1;37m\]'
+# blue='\[\033[0;34m\]'
+# BLUE='\[\033[1;34m\]'
+# cyan='\[\033[0;36m\]'
+# CYAN='\[\033[1;36m\]'
+# NC='\[\033[0m\]'
